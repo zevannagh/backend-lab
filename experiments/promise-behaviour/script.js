6723d965e21b853258009f08a2e170f6a3128e9a
Zevanna <zevannagh@users.noreply.github.com>
2026-01-15 23:17:50 +0700

    experiment: observe promise execution order and microtask queue

--- experiments/promise-behaviour/script.js
+++ experiments/promise-behaviour/script.js
@@ -1,1 +1,17 @@
-//Building Simple Promise 
+//Building Simple Promise
+
+// 1.
+console.log("Start..");
+Promise.resolve("Hello World").then((value) => console.log(value));
+console.log("End..");
+
+// 2.
+console.log("A");
+
+setTimeout(() => console.log("B"), 0);
+
+Promise.resolve()
+	.then(() => console.log("C"))
+	.then(() => console.log("D"));
+
+console.log("E");
