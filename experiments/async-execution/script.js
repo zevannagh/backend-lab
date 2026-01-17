1c18c16488716463da0e8f398b18771bfee82337
Zevanna <zevannagh@users.noreply.github.com>
2026-01-17 23:46:42 +0700

    experiment: observe sync execution before setTimeout callback

--- experiments/async-execution/script.js
+++ experiments/async-execution/script.js
@@ -1,1 +1,8 @@
-// Async Execution  
+// Async Execution
+
+// 1
+console.log("Start.."); // sync → call stack
+setTimeout(() => {
+	console.log("Async Task Completed"); // daftar async → call stack
+}, 0);
+console.log("Continue.."); // sync → call stack
